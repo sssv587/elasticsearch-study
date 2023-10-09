1e89e8c1a9cac7a004e9a2043e969dfd7f83ef9c
sssv587 <1092663295@qq.com>
2023-10-10 00:45:08 +0800

    修改文档

---
+++ src/main/java/com/futurebytedance/doc/ES_Doc_Update.java
@@ -0,0 +1,39 @@
+package com.futurebytedance.doc;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/10/10 - 0:41
+ * @Description 文档-修改
+ */
+public class ES_Doc_Update {
+    public static void main(String[] args) throws IOException {
+        // 创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        // 修改数据
+        UpdateRequest request = new UpdateRequest();
+
+        request.index("user").id("1001");
+        request.doc(XContentType.JSON, "sex", "女");
+
+        UpdateResponse response = esClient.update(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getResult());
+
+        // 关闭ES客户端
+        esClient.close();
+    }
+}
