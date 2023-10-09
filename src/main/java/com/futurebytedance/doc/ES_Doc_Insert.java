65165cb59d675406481f6e333f72e93c27f97ed7
sssv587 <1092663295@qq.com>
2023-10-10 00:41:11 +0800

    添加文档

---
+++ src/main/java/com/futurebytedance/doc/ES_Doc_Insert.java
@@ -0,0 +1,47 @@
+package com.futurebytedance.doc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/10/10 - 0:34
+ * @Description 文档-新增
+ */
+public class ES_Doc_Insert {
+    public static void main(String[] args) throws IOException {
+        // 创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        // 插入数据
+        IndexRequest request = new IndexRequest();
+        request.index("user").id("1001");
+
+        User user = new User();
+        user.setName("zhangsan");
+        user.setAge(30);
+        user.setSex("男");
+
+        // 向ES插入数据，必须将数据转换为JSON格式
+        ObjectMapper mapper = new ObjectMapper();
+        String userJson = mapper.writeValueAsString(user);
+        request.source(userJson, XContentType.JSON);
+        IndexResponse response = esClient.index(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getResult());
+
+        // 关闭ES客户端
+        esClient.close();
+    }
+}
