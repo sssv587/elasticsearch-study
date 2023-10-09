3d83776b49d162775257c482b7f21cb7ac90af2f
sssv587 <1092663295@qq.com>
2023-10-10 00:27:42 +0800

    查询索引

---
+++ src/main/java/com/futurebytedance/index/ES_Index_Search.java
@@ -0,0 +1,37 @@
+package com.futurebytedance.index;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/10/10 - 0:23
+ * @Description 索引-查询
+ */
+public class ES_Index_Search {
+    public static void main(String[] args) throws IOException {
+        // 创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        // 查询索引
+        GetIndexRequest request = new GetIndexRequest("user");
+        GetIndexResponse getIndexResponse = esClient.indices().get(request, RequestOptions.DEFAULT);
+
+        // 相应状态
+        System.out.println(getIndexResponse.getAliases());
+        System.out.println(getIndexResponse.getMappings());
+        System.out.println(getIndexResponse.getSettings());
+
+        // 关闭ES客户端
+        esClient.close();
+    }
+}
