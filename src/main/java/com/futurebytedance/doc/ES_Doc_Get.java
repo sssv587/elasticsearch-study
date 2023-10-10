8a137d5deba168b0358dc18972ccbc135b71bfef
sssv587 <1092663295@qq.com>
2023-10-10 23:51:39 +0800

    查询文档

---
+++ src/main/java/com/futurebytedance/doc/ES_Doc_Get.java
@@ -0,0 +1,34 @@
+package com.futurebytedance.doc;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/10/10 - 23:48
+ * @Description 文档-查找
+ */
+public class ES_Doc_Get {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        // 查询数据
+        GetRequest request = new GetRequest();
+        request.index("user").id("1001");
+        GetResponse response = esClient.get(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getSourceAsString());
+
+        // 关闭ES客户端
+        esClient.close();
+    }
+}
