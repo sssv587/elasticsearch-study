a9ecd862f7dd6b6fba3abe2ab52a6f1969ffd476
sssv587 <1092663295@qq.com>
2023-10-11 00:09:02 +0800

    文档查询-全量查询

---
+++ src/main/java/com/futurebytedance/doc/query/ES_Doc_Query.java
@@ -0,0 +1,47 @@
+package com.futurebytedance.doc.query;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2023/10/11 - 0:01
+ * @Description 查询文档
+ */
+public class ES_Doc_Query {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        // 1.查询索引中全部的数据
+        SearchRequest request = new SearchRequest();
+        request.indices("user");
+
+        request.source(new SearchSourceBuilder().query(QueryBuilders.matchAllQuery()));
+        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+        System.out.println(response.getTook());
+
+        for (SearchHit hit : hits) {
+            System.out.println(hit.getSourceAsString());
+        }
+
+        esClient.close();
+    }
+}
