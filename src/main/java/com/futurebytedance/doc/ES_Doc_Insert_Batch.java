e8b95d6aee2eb1df2836f6b82d07c76609eeb11e
sssv587 <1092663295@qq.com>
2023-10-11 00:01:11 +0800

    批量插入文档

--- src/main/java/com/futurebytedance/doc/ES_Doc_Insert_Batch.java
+++ src/main/java/com/futurebytedance/doc/ES_Doc_Insert_Batch.java
@@ -28,9 +28,12 @@ public class ES_Doc_Insert_Batch {
         // 批量插入数据
         BulkRequest request = new BulkRequest();
 
-        request.add(new IndexRequest().index("user").id("1001").source(XContentType.JSON, "name", "zhangsan"));
-        request.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON, "name", "lisi"));
-        request.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON, "name", "wangwu"));
+        request.add(new IndexRequest().index("user").id("1001").source(XContentType.JSON, "name", "zhangsan", "age", 30, "sex", "男"));
+        request.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON, "name", "lisi", "age", 30, "sex", "女"));
+        request.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON, "name", "wangwu", "age", 40, "sex", "男"));
+        request.add(new IndexRequest().index("user").id("1004").source(XContentType.JSON, "name", "wangwu1", "age", 40, "sex", "女"));
+        request.add(new IndexRequest().index("user").id("1005").source(XContentType.JSON, "name", "wangwu2", "age", 50, "sex", "男"));
+        request.add(new IndexRequest().index("user").id("1006").source(XContentType.JSON, "name", "wangwu3", "age", 50, "sex", "男"));
 
         BulkResponse response = esClient.bulk(request, RequestOptions.DEFAULT);
         System.out.println(response.getTook());
